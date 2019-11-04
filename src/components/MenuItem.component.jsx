280fc90dd548bdcb669bec5c4e4970310da090cf
keralex <kerlis.aguado26@gmail.com>
2019-11-04 20:27:13 -0300

    style

---
+++ src/components/MenuItem.component.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MenuItem.component extends Component {
+    render() {
+        return (
+            <div>s
+                
+            </div>
+        );
+    }
+}
+
+export default MenuItem.component;
