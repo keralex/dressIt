4bb3926ddae5ac2c22ec9042341753e8121a7539
keralex <kerlis.aguado26@gmail.com>
2019-11-09 21:18:29 -0300

    shoppage

---
+++ src/pages/shop/shopPage.component.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class shopPage extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default shopPage;
