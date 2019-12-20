98d82757f710e8d7cf62ce14a1d1d14d6ebf926b
keralex <kerlis.aguado26@gmail.com>
2019-12-20 11:24:15 -0300

    signin

--- src/components/sign-in/signIn.component.jsx
+++ src/components/sign-in/signIn.component.jsx
@@ -8,10 +8,26 @@ class signIn extends Component {
             password:''
         }
     }
+    handleSubmit=event=>{
+        event.preventDefault();
+        this.setState({email:'', password:''})
+    }
+    hangelChange=event =>{
+        const {value,name}=event.target;
+        this.setState({[name]:value})
+    }
     render() {
         return (
-            <div>
-                
+            <div className='sign-in'>
+                <h2>I Already have an account</h2>
+                <span>Sign in with your email and password</span> 
+                <form onSubmit={this.handleSubmit} >
+                    <input name='email' type='email' value={this.state.email} onChange={this.hangelChange} required />
+                    <label>Email</label>
+                    <input name='password' type='password' value={this.state.password} onChange={this.hangelChange} required />
+                    <label>Password</label>
+                    <input type='submit' value='Submit Form' />
+                </form>
             </div>
         );
     }
--- src/pages/Sign-in-up-page/signPage.component.jsx
+++ src/pages/Sign-in-up-page/signPage.component.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import SignIn from '../../components/sign-in/signIn.component';
 const SignPage = () => (
     <div className='sign-in-and-sign-up'>
-Sign Page
+<SignIn/>
     </div>
 
 
